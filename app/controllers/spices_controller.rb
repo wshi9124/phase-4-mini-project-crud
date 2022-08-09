45b71a1154c890fd85527667c73c9c737e11d82b
Willie Shi <wshi9124@gmail.com>
2022-08-09 18:59:16 -0400

    commit tree b652debdce69e9225515b43256cb07cbb447d608

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,50 @@
+class SpicesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    def index
+        spices= Spice.all
+        render json: spices
+    end
+
+    def show
+        spice= find_spice
+        render json: spice
+    end
+
+    def create
+        spice= Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice= find_spice
+        spice.update(spice_params)
+        render json: spice
+    end
+
+    def increment_likes
+        spice= find_spice
+        spice.update(likes: bird.likes +1)
+        render json: bird 
+    end 
+
+    def destroy
+        spice= find_spice
+        spice.destroy
+        head :no_content
+        # or render json: {}
+    end
+
+    private
+
+    def find_spice
+        Spice.find(params[:id])
+    end
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    def render_not_found_response
+        render json: { error: "Spice not found" }, status: :not_found 
+    end
+end
